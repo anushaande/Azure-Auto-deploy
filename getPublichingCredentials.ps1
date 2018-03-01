4524285d5d52c650a32d4783c686bf4ea1180adf
anushaande <36480151+anushaande@users.noreply.github.com>
2018-03-01 12:45:55 -0500

    Adding additional tabs in navigation bar

--- getPublichingCredentials.ps1
+++ getPublichingCredentials.ps1
@@ -2,6 +2,10 @@
 $resourceGroupName = "USFTestCI"
 $webAppName = "USFTestCI"
 function Get-AzureRmWebAppPublishingCredentials($resourceGroupName, $webAppName, $slotName = $null){
+
+$resourceGroupName = "USFTestCI"
+$webAppName = "USFTestCI"
+
 	if ([string]::IsNullOrWhiteSpace($slotName)){
 		$resourceType = "Microsoft.Web/sites/config"
 		$resourceName = "$webAppName/publishingcredentials"
@@ -13,4 +17,69 @@ function Get-AzureRmWebAppPublishingCredentials($resourceGroupName, $webAppName,
 	$publishingCredentials = Invoke-AzureRmResourceAction -ResourceGroupName $resourceGroupName -ResourceType $resourceType -ResourceName $resourceName -Action list -ApiVersion 2015-08-01 -Force
     	return $publishingCredentials
 }
-Get-AzureRmWebAppPublishingCredentials
+
+function Get-KuduApiAuthorisationHeaderValue($resourceGroupName, $webAppName, $slotName = $null){
+
+$resourceGroupName = "USFTestCI"
+$webAppName = "USFTestCI"
+
+    $publishingCredentials = Get-AzureRmWebAppPublishingCredentials $resourceGroupName $webAppName $slotName
+    return ("Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $publishingCredentials.Properties.PublishingUserName, $publishingCredentials.Properties.PublishingPassword))))
+}
+
+
+# function Download-FileFromWebApp($resourceGroupName, $webAppName, $slotName = "", $kuduPath, $localPath){
+
+# $resourceGroupName = "USFTestCI"
+# $webAppName = "USFTestCI"
+# $kuduPath = "getPublichingCredentials.ps1"
+# $localPath = "Documents"
+
+    # $kuduApiAuthorisationToken = Get-KuduApiAuthorisationHeaderValue $resourceGroupName $webAppName $slotName
+    # if ($slotName -eq ""){
+        # $kuduApiUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/$kuduPath"
+    # }
+    # else{
+        # $kuduApiUrl = "https://$webAppName`-$slotName.scm.azurewebsites.net/api/vfs/site/wwwroot/$kuduPath"
+    # }
+    # $virtualPath = $kuduApiUrl.Replace(".scm.azurewebsites.", ".azurewebsites.").Replace("/api/vfs/site/wwwroot", "")
+    # Write-Host " Downloading File from WebApp. Source: '$virtualPath'. Target: '$localPath'..." -ForegroundColor DarkGray
+
+    # Invoke-RestMethod -Uri $kuduApiUrl `
+                        # -Headers @{"Authorization"=$kuduApiAuthorisationToken;"If-Match"="*"} `
+                        # -Method GET `
+                        # -OutFile $localPath `
+                        # -ContentType "multipart/form-data"
+# }
+
+# Download-FileFromWebApp
+
+function Download-FileFromWebApp($resourceGroupName, $webAppName, $slotName = "", $kuduPath){
+
+$resourceGroupName = "USFTestCI"
+$webAppName = "USFTestCI"
+$kuduPath = "getPublichingCredentials.ps1~"
+
+    $kuduApiAuthorisationToken = Get-KuduApiAuthorisationHeaderValue $resourceGroupName $webAppName $slotName
+    if ($slotName -eq ""){
+        $kuduApiUrl = "https://$webAppName.scm.azurewebsites.net/api/vfs/site/wwwroot/$kuduPath"
+    }
+    else{
+        $kuduApiUrl = "https://$webAppName`-$slotName.scm.azurewebsites.net/api/vfs/site/wwwroot/$kuduPath"
+    }
+    $virtualPath = $kuduApiUrl.Replace(".scm.azurewebsites.", ".azurewebsites.").Replace("/api/vfs/site/wwwroot", "")
+    #Write-Host " Downloading File from WebApp. Source: '$virtualPath'. Target: '$localPath'..." -ForegroundColor DarkGray
+	Write-Host "Deleting the file '$KuduPath' at Target: '$kuduApiUrl'..." -ForegroundColor DarkGray
+    Invoke-RestMethod -Uri $kuduApiUrl `
+                        -Headers @{"Authorization"=$kuduApiAuthorisationToken;"If-Match"="*"} `
+                        -Method DELETE `
+						-ContentType "multipart/form-data"
+                        
+}
+
+Download-FileFromWebApp
+
+
+
+
+
