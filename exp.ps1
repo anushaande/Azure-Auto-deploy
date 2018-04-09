21ab12c947343670b215780b250cf79b3bc5298a
anushaande <36480151+anushaande@users.noreply.github.com>
2018-04-09 16:20:18 -0400

    testing script

--- exp.ps1
+++ exp.ps1
@@ -3,6 +3,7 @@
 
 
 $Files_Committed = New-Object System.Collections.ArrayList
+$Files_Selected = New-Object System.Collections.ArrayList
 
 # Get the files changed in the most recent commit and convert them as an ArrayList
 $Commit_id = git log --format="%H" -n 1
@@ -12,6 +13,8 @@ $Files_Committed = git diff-tree --no-commit-id --name-only -r $Commit_id | Form
 $id_to_String = $Commit_id | out-string
 $Patch_id = "Patch" + $id_to_String.Trim() 
 $Patch_name = $Patch_id + ".patch"
-git format-patch -1 Head Additional.html,index.html --stdout > $Patch_name
+#$Files_Selected = [ Additional.html , index.html ]
+$Files_Selected = "Additional.html" , "index.html"
+git format-patch -1 Head $Files_Selected --stdout > $Patch_name
 
-echo $Patch_name
+cat $Patch_name
