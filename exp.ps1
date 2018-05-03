b809e844b2b1b5892a336e857e6725d170f5660c
anushaande <anushaande@health.usf.edu>
2018-05-03 16:33:20 -0400

    Commiting to sync with master

--- exp.ps1
+++ exp.ps1
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 
+$Commited_list = New-Object System.Collections.ArrayList
+# $Files_Committed = New-Object System.Collections.ArrayList
+# $Files_Selected = New-Object System.Collections.ArrayList tgrcfd3he files changed in the most recent commit and convert them as an ArrayList
+$Commited_list = git log --format="%H" --after="2018-4-1 11:00:00 AM" --before="Tuesday, May 1, 2018 3:03:38 PM"
+#$Files_Committed = git diff-tree --no-commit-id --name-only -r $Commit_id | Format-list
+$i=1
+foreach($commit in $Commited_list)
+{
 
-$Files_Committed = New-Object System.Collections.ArrayList
-$Files_Selected = New-Object System.Collections.ArrayList
+echo "commit id $i is $commit"
+$i = $i+1
 
-# Get the files changed in the most recent commit and convert them as an ArrayList
-$Commit_id = git log --format="%H" -n 1
-$Files_Committed = git diff-tree --no-commit-id --name-only -r $Commit_id | Format-list
+}
 
-#Create a patch with commit id.
-$id_to_String = $Commit_id | out-string
-$Patch_id = "Patch" + $id_to_String.Trim() 
-$Patch_name = $Patch_id + ".patch"
-#$Files_Selected = [ Additional.html , index.html ]
-$Files_Selected = "Additional.html" , "index.html"
-git format-patch -1 Head $Files_Selected --stdout > $Patch_name
-
-cat $Patch_name
