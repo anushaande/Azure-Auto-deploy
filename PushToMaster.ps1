f850994342f699273a58ec38a2821a32ac42a0be
anushaande <anushaande@health.usf.edu>
2018-05-04 11:39:19 -0400

    Commiting changes of PushToMaster

--- PushToMaster.ps1
+++ PushToMaster.ps1
@@ -1,15 +1,16 @@
 #!/bin/bash
 write-host "--------------------------------------Adding changes in dev-branch to master branch-----------------------------------------" -ForegroundColor "Yellow" -BackgroundColor "DarkGreen"
 
-git checkout dev-branch
+git checkout master
 rm PatchOfDiff -Recurse -ErrorAction SilentlyContinue
-git diff dev-branch master > PatchOfDiff
+git diff master dev-branch > PatchOfDiff
 cat PatchOfDiff | git apply --reject --ignore-whitespace --whitespace=nowarn
 rm PatchOfDiff
+git add .
 $date = Date
 git commit -m "Commiting changes on $date"
-git push -u origin master
-rm .\.git\rebase-apply -Recurse -ErrorAction SilentlyContinue"
+git push -u origin master 
+rm .\.git\rebase-apply -Recurse -ErrorAction SilentlyContinue
 git am --skip
 git status
 
