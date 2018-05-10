58d698113a11b9dca77704047cfd90274c6bf634
anushaande <anushaande@health.usf.edu>
2018-05-10 11:32:03 -0400

    commit11

--- PushToMaster.ps1
+++ PushToMaster.ps1
@@ -1,25 +1,19 @@
 #!/bin/bash
 write-host "--------------------------------------Adding changes in dev-branch to master branch-----------------------------------------" -ForegroundColor "Yellow" -BackgroundColor "DarkGreen"
-write-host "----------------------------------CHecking out to master------------------------------------------------" -ForegroundColor "Cyan"
+write-host "----------------------------------Checking to master-------------------------------------------------------" -ForegroundColor "Cyan"
 git checkout master
-write-host "----------------------------------Deleting the previous patch if any------------------------------------------------" -ForegroundColor "Cyan"
 rm PatchOfDiff -Recurse -ErrorAction SilentlyContinue
-write-host "----------------------------------Check differences between dev and master branch------------------------------------------------" -ForegroundColor "Cyan"
 git diff master dev-branch > PatchOfDiff
-write-host "----------------------------------Applying patch in master------------------------------------------------" -ForegroundColor "Cyan"
+write-host "----------------------------------Applying changes in dev-branch to master---------------------------------" -ForegroundColor "Cyan"
 cat PatchOfDiff | git apply --reject --ignore-whitespace --whitespace=nowarn
-write-host "----------------------------------Removing the patch which is applied------------------------------------------------" -ForegroundColor "Cyan"
 rm PatchOfDiff
 git add .
-write-host "----------------------------------Commiting the changes in master branch------------------------------------------------" -ForegroundColor "Cyan"
+write-host "----------------------------------Committing the changes in master branch----------------------------------" -ForegroundColor "Cyan"
 $date = Date
-git commit -m "Commiting changes on $date"
-write-host "----------------------------------Pushing changes to master branch------------------------------------------------" -ForegroundColor "Cyan"
+git commit -m "Committing changes on $date"
+write-host "----------------------------------Pushing changes to master branch-----------------------------------------" -ForegroundColor "Cyan"
 git push -u origin master 
 rm .\.git\rebase-apply -Recurse -ErrorAction SilentlyContinue
-write-host "----------------------------------Skipping the last patch if any errors------------------------------------------------" -ForegroundColor "Cyan"
-git am --skip
-write-host "----------------------------------Checking for git status------------------------------------------------" -ForegroundColor "Cyan"
+write-host "----------------------------------Git status---------------------------------------------------------------" -ForegroundColor "Cyan"
 git status
-write-host "----------------------------------Checking for differences between master and dev-branch now-------------------------" -ForegroundColor "Cyan"
-git diff master dev-branch
+
