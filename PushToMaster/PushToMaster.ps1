c0a07c3db57ca1bba1bd8c177efe8d4f8ddc8123
anushaande <anushaande@health.usf.edu>
2018-05-23 11:15:25 -0400

    commiting changes

---
+++ PushToMaster/PushToMaster.ps1
@@ -0,0 +1,29 @@
+<#   
+================================================================================ 
+ Name:         PushToMaster.ps1 
+ Purpose:      Replicate all the changes of dev-branch to master. 
+ Author:       Anusha Ande – anushaande@health.usf.edu
+ Description: 
+ 
+ Syntax/Execution: Execute this script having all the powershell files in a single 
+ folder 
+ Disclaimer:      Follow all the instructions carefully. Else, there is a high chance 
+                 that code gets spoiled.
+ Limitations:  
+
+# video of Script:
+ 
+ ================================================================================ 
+ #>
+ #!/bin/bash
+
+ ."C:\WorkStation\GitLocalRepo\Azure-Auto-deploy\GroupFiles.ps1"
+ 
+ Add-Type -Assembly System.Windows.Forms     ## Load the Windows Forms assembly 
+ ## Create the main form 
+ Write-Host "Create form" (Get-Date)  
+ $form = New-Object Windows.Forms.Form 
+$form.FormBorderStyle = "FixedToolWindow" 
+$form.Text = "PowerShell Custom GUI Window" 
+$form.StartPosition = "CenterScreen" 
+$form.Width = 740 ; $form.Height = 380  # Make the form wider 
